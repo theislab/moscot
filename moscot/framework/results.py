e8fb37160bda509f633b6bd1089a8a1bd6598f14
Dominik Klein <domin.klein@gmail.com>
2021-12-21 12:18:27 +0100

    changed structure so multiple OT problems are handled with dictionaries

--- moscot/framework/results.py
+++ moscot/framework/results.py
@@ -18,23 +18,44 @@ CostFn_t = Union[CostFn, GWLoss]
 class BaseResult(ABC):
     """Base result for OT problems."""
 
-    def __init__(self, adata: AnnData, _estimators: Union[List['BaseProblem'], 'BaseProblem']) -> None:
-        self.adata = adata
-        self._estimators = _estimators
+    def __init__(self,
+                 adata: AnnData,
+                 estimators_dict: Dict[Tuple, 'BaseProblem']) -> None:
+        self._adata = adata
+        self._estimators = estimators_dict
+        self._groups = None
 
-    def group_by(self, key: str):
-        pass
+    def group_cells(self, key: Union[str, List[str]]) -> List:
+        if isinstance(key, str):
+            adata_groups = self._adata.obs[key].values
+            assert key in adata_groups
+            self._groups = list(adata_groups)
+        else:
+            self._groups = key
+
+    @property
+    def adata(self) -> AnnData:
+        return self._adata
+
+    @property
+    def estimators(self) -> Union[List['BaseProblem'], 'BaseProblem']:
+        return self._estimators
 
 
 class OTResult(BaseResult):
-    def __init__(self, adata: AnnData, _estimators: Union[List['BaseProblem'], 'BaseProblem']) -> None:
-        super().__init__(adata=adata, _estimators=_estimators)
-
+    def __init__(self, adata: AnnData,
+                 estimators_dict: Dict[Tuple, 'BaseProblem']) -> None:
+        self.matrix_dict = {tup: getattr(estimator, "matrix") for tup, estimator in estimators_dict.items()}
+        super().__init__(adata=adata, estimators=estimators_dict)
 
     def plot_aggregate_transport(self, key: str, groups: List[str]):
         pass
 
-    def push_forward(self, start, end, key, groups):
+    def push_forward(self,
+                     start,
+                     end,
+                     key,
+                     groups):
         pass
 
     def pull_backward(self, start, end, keys, groups):
