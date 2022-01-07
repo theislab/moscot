d07f5d541e39b81a841ec4263719d69d9934066d
Dominik Klein <domin.klein@gmail.com>
2022-01-07 11:34:40 +0100

    restructured the result mixin class

--- moscot/framework/BaseProblem.py
+++ moscot/framework/BaseProblem.py
@@ -1,17 +1,8 @@
+# TODO: This file should be independent of backend, e.g. JAX
 from abc import abstractmethod
 from typing import Any, Dict, List, Tuple, Union, Optional
-from numbers import Number
-from jax import numpy as jnp
-from ott.geometry.costs import CostFn
-from ott.geometry.geometry import Geometry
-from ott.core.gromov_wasserstein import GWLoss
-
-CostFn_t = Union[CostFn, GWLoss]
+import numpy as np
 from sklearn.base import BaseEstimator
-
-from ott.geometry.costs import Euclidean
-from ott.geometry.epsilon_scheduler import Epsilon
-
 from anndata import AnnData
 
 
@@ -22,39 +13,31 @@ class BaseProblem(BaseEstimator):
     def __init__(
         self,
         adata: AnnData = None,
-        rep: str = "X",
     ) -> None:
         self._adata = adata
-        self._rep = rep
 
     @abstractmethod
     def prepare(
-        self,
-        key: Union[str, None],
-        policy: None,
-        rep: None,
-        cost_fn: Union[CostFn, None],
-        eps: Union[float, None],
-        groups: Union[List[str], Tuple[str]],
+        self
     ) -> "BaseProblem":
         pass
 
     @abstractmethod
-    def fit(
+    def solve(
         self,
-        epsilon: Optional[Union[List[Union[float, Epsilon]], float, Epsilon]] = 0.5,
-        tau_a: Optional[Union[List, Dict[Tuple, List], float]] = 1.0,
-        tau_b: Optional[Union[List, Dict[Tuple, List], float]] = 1.0,
-    ) -> "BaseResult":
+        inplace: bool = True
+    ) -> Optional["BaseResult"]:
+        pass
+
+    @abstractmethod
+    def prepare(
+            self
+    ) -> "BaseProblem":
         pass
 
     @property
     def adata(self) -> AnnData:
         return self._adata
-
-    @property
-    def rep(self) -> str:
-        return self._rep
 
     @property
     @abstractmethod
@@ -68,7 +51,7 @@ class BaseProblem(BaseEstimator):
 
     @property
     @abstractmethod
-    def transport_matrix(self) -> Dict[Tuple, jnp.ndarray]:
+    def transport_matrix(self) -> Dict[Tuple, np.ndarray]:
         pass
 
 
--- moscot/framework/results.py
+++ moscot/framework/results.py
@@ -4,72 +4,52 @@ from numbers import Number
 from networkx import DiGraph
 from abc import ABC, abstractmethod
 
-from jax import numpy as jnp
 from ott.geometry.costs import CostFn
 from ott.geometry.geometry import Geometry
 from ott.core.gromov_wasserstein import GWLoss
 import numpy as np
 from anndata import AnnData
 
+
+from moscot.framework._result_cls import MatrixOTResult, PotentialOTResult, OTResult
+
 CostFn_t = Union[CostFn, GWLoss]
 
 
 
-class BaseResult(ABC):
-    """Base result for OT problems."""
 
+class ResultMixin(ABC):
+    """ Mixin class for OT estimators handling downstream functions """
     def __init__(self,
                  adata: AnnData,
-                 key: str,
-                 estimators_dict: Dict[Tuple, 'BaseProblem']) -> None:
-        self._adata = adata
-        self._key = key
-        self._estimators_dict = estimators_dict
+                 otResult: Union[OTResult, "from_matrix", "from_potentials"],
+                 ) -> None:
 
-    def group_cells(self, key: Union[str, List[str]]) -> List:
-        if isinstance(key, str):
-            adata_groups = self._adata.obs[key].values
-            assert key in adata_groups
-            self._groups = list(adata_groups)
+        self._adata = adata  # @TODO: dicuss if we want this attribute. MUCDK: I like it because this way the adata
+        #TODO: object only has to be passed on once as it is needed in multiple functions. Computational aspects? Please lmk
+        if otResult == "from_matrix":
+            self._otResult = MatrixOTResult
+        elif otResult == "from_potentials":
+            self._otResult = PotentialOTResult
         else:
-            self._groups = key
+            self._otResult = otResult
+
+    def __getattr__(self, item):  # https://stackoverflow.com/questions/65754399/conditional-inheritance-based-on-arguments-in-python
+        return self._otResult.__getattribute__(item)
 
     @property
     def adata(self) -> AnnData:
         return self._adata
 
-    @property
-    def estimators_dict(self) -> Union[List['BaseProblem'], 'BaseProblem']:
-        return self._estimators_dict
-
-    @staticmethod
-    def _push_forward(mass: jnp.ndarray,
-                      matrix: jnp.ndarray) -> jnp.ndarray:
-        return jnp.dot(mass, matrix)
-
-    @staticmethod
-    def _pull_back(mass: jnp.ndarray,
-                   matrix: jnp.ndarray) -> jnp.ndarray:
-        return jnp.transpose(jnp.dot(matrix, mass))
-
-
-class OTResult(BaseResult):
-    def __init__(self,
-                 adata: AnnData,
-                 key: str,
-                 estimators_dict: Dict[Tuple, 'BaseProblem']) -> None:
-        self.matrix_dict = {tup: getattr(estimator, "matrix") for tup, estimator in estimators_dict.items()}
-        super().__init__(adata=adata, key=key, estimators_dict=estimators_dict)
-
     def plot_aggregate_transport(self, key: str, groups: List[str]):
         pass
 
     def push_forward_composed(self,
-                              start: int,
-                              end: int,
+                              start: Any,
+                              end: Any,
                               key_groups: Optional[str] = None,
                               groups: Optional[List[str]] = None
-                              ) -> List[jnp.ndarray]:
+                              ) -> List[np.ndarray]:
         if start > end:
             raise ValueError("push_forward_composed() requires start > end.")
         self._verify_sequence_exists(start, end)
@@ -80,11 +60,11 @@ class OTResult(BaseResult):
         return masses
 
     def pull_back_composed(self,
-                           start: int,
-                           end: int,
+                           start: Any,
+                           end: Any,
                            key_groups: Optional[str] = None,
                            groups: Optional[List[str]] = None
-                           ) -> List[jnp.ndarray]:
+                           ) -> List[np.ndarray]:
         if start < end:
             raise ValueError("pull_back_composed() requires start < end.")
         self._verify_sequence_exists(end, start)
@@ -95,12 +75,12 @@ class OTResult(BaseResult):
         return masses
 
     def push_forward(self,
-                     start: int, #TODO: check what type the key values should be
-                     end: int,
+                     start: Any, #TODO: check what type the key values should be
+                     end: Any,
                      key_groups: Optional[str] = None,
                      groups: Optional[List[str]] = None,
                      mass: Optional[np.ndarray] = None,
-                     ) -> List[jnp.ndarray]:
+                     ) -> List[np.ndarray]:
 
         if start > end:
             raise ValueError("push_forward() requires start > end.")
@@ -114,12 +94,12 @@ class OTResult(BaseResult):
         return [mass, self._push_forward(mass, matrix)]
 
     def pull_back(self,
-                  start: int, #TODO: check what type the key values should be
-                  end: int,
+                  start: Any,
+                  end: Any,
                   key_groups: Optional[str] = None,
                   groups: Optional[List[str]] = None,
                   mass: Optional[np.ndarray] = None,
-                  ) -> List[jnp.ndarray]:
+                  ) -> List[np.ndarray]:
         if start < end:
             raise ValueError("pull_back() requires start < end.")
         if (end, start) not in self.matrix_dict.keys():
@@ -130,13 +110,14 @@ class OTResult(BaseResult):
         else:
             self._verify_mass(matrix, mass, 1)
         return [mass, self._pull_back(mass, matrix)]
-
-    @staticmethod
-    def _verify_mass(matrix, mass, dimension):
-        if len(mass.shape) != 1:
-            raise ValueError("The dimension of provided mass must be 1.")
-        if matrix.shape[dimension] == len(mass):
-            raise ValueError("The dimensions of the matrix and the mass do not match.")
+    
+    def group_cells(self, key: Union[str, List[str]]) -> List:
+        if isinstance(key, str):
+            adata_groups = self._adata.obs[key].values
+            assert key in adata_groups
+            self._groups = list(adata_groups)
+        else:
+            self._groups = key
 
     def _prepare_transport(self,
                            matrix,
@@ -153,10 +134,10 @@ class OTResult(BaseResult):
             return self._make_mass_from_cells(source, groups, key_groups)
         else:
             dim_0 = matrix.shape[0]
-            return jnp.full(dim_0, 1/dim_0)
+            return np.full(dim_0, 1/dim_0)
 
     def _verify_cells_are_present(self,
-                                  start: int,
+                                  start: Any,
                                   groups: List[str],
                                   key_groups: str) -> None:
         adata_groups = set(self._adata[self._adata.obs[self._key] == start][self._adata.obs[key_groups]].values)
@@ -165,13 +146,13 @@ class OTResult(BaseResult):
                 raise ValueError("Group {} is not present for considered data point {}".format(group, start))
 
     def _make_mass_from_cells(self,
-                              start: int,
+                              start: Any,
                               groups: List[str],
-                              key_groups: str) -> jnp.ndarray:
+                              key_groups: str) -> np.ndarray:
         groups_at_start = self._adata[self._adata.obs[self._key] == start].obs[key_groups].values
-        mass = jnp.zeros(len(groups_at_start))
+        mass = np.zeros(len(groups_at_start))
         in_group = [group in groups for group in groups_at_start]
-        n_cells = jnp.sum(in_group)
+        n_cells = np.sum(in_group)
         mass[in_group] = 1/n_cells
         return mass
 
@@ -182,5 +163,14 @@ class OTResult(BaseResult):
         for i in range(early, late):
             if (i, i+1) not in matrix_keys:
                 raise ValueError("No transport matrix was calculated for {}".format((i, i+1)))
+
+    @staticmethod
+    def _verify_mass(matrix, mass, dimension):
+        if len(mass.shape) != 1:
+            raise ValueError("The dimension of provided mass must be 1.")
+        if matrix.shape[dimension] == len(mass):
+            raise ValueError("The dimensions of the matrix and the mass do not match.")
+
+
 
 
