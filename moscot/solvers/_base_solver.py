9d54becf145e77dfed3fce0a02eceb36eb62dd70
michalk8 <46717574+michalk8@users.noreply.github.com>
2022-02-17 18:07:09 +0000

    Clean up prepare/solve kwargs, update alpha (#94)

--- moscot/solvers/_base_solver.py
+++ moscot/solvers/_base_solver.py
@@ -1,5 +1,6 @@
 from abc import ABC, abstractmethod
-from typing import Any, Union, Optional
+from types import MappingProxyType
+from typing import Any, Union, Mapping, Optional
 import warnings
 
 import numpy.typing as npt
@@ -19,8 +20,7 @@ class BaseSolver(ABC):
         self,
         x: TaggedArray,
         y: Optional[TaggedArray] = None,
-        a: npt.ArrayLike = None,
-        b: npt.ArrayLike = None,
+        eps: Optional[float] = None,
         **kwargs: Any,
     ) -> Any:
         pass
@@ -40,31 +40,21 @@ class BaseSolver(ABC):
         tau_a: float = 1.0,
         tau_b: float = 1.0,
         eps: Optional[float] = None,
-        **kwargs: Any,
+        solve_kwargs: Mapping[str, Any] = MappingProxyType({}),
+        **prepare_kwargs: Any,
     ) -> BaseSolverOutput:
-        def to_tagged_array(arr: Optional[ArrayLike], tag: Tag) -> Optional[TaggedArray]:
-            if arr is None:
-                return None
-            tag = Tag(tag)
-            if not isinstance(arr, TaggedArray):
-                return TaggedArray(arr, tag=tag)
-            # force new tag
-            return TaggedArray(arr.data, tag=tag)
-
         # currently we don't provide x_tag as kwarg, hence we would always convert tags here
         if not isinstance(x, TaggedArray):
-            x = to_tagged_array(x, kwargs.pop("x_tag", Tag.POINT_CLOUD))
+            x = _to_tagged_array(x, prepare_kwargs.pop("x_tag", Tag.POINT_CLOUD))
         if not isinstance(y, TaggedArray):
-            y = to_tagged_array(y, kwargs.pop("y_tag", Tag.POINT_CLOUD))
+            y = _to_tagged_array(y, prepare_kwargs.pop("y_tag", Tag.POINT_CLOUD))
         if not isinstance(xx, TaggedArray):
-            xx = to_tagged_array(xx, kwargs.pop("xx_tag", Tag.COST_MATRIX) if yy is None else Tag.POINT_CLOUD)
+            xx = _to_tagged_array(xx, prepare_kwargs.pop("xx_tag", Tag.COST_MATRIX) if yy is None else Tag.POINT_CLOUD)
         if not isinstance(yy, TaggedArray):
-            yy = to_tagged_array(yy, kwargs.pop("yy_tag", Tag.POINT_CLOUD))
+            yy = _to_tagged_array(yy, prepare_kwargs.pop("yy_tag", Tag.POINT_CLOUD))
 
-        # TODO(michalk8): create TaggedArray here if not passed, taking x_tag/y_tag/xy_tag from kwargs
-        # TODO(michak8): filter kwargs
-        data = self._prepare_input(x, y, a, b, xx=xx, yy=yy, tau_a=tau_a, tau_b=tau_b, eps=eps)
-        res = self._solve(data, **kwargs)
+        data = self._prepare_input(x, y, eps=eps, xx=xx, yy=yy, a=a, b=b, tau_a=tau_a, tau_b=tau_b, **prepare_kwargs)
+        res = self._solve(data, **solve_kwargs)
 
         if not res.converged:
             warnings.warn("Solver did not converge")
@@ -76,3 +66,14 @@ class BaseSolver(ABC):
             _warn_not_close((res._ones(m) / m) if b is None else b, res.b, kind="target")
 
         return res
+
+
+def _to_tagged_array(arr: Optional[ArrayLike], tag: Tag) -> Optional[TaggedArray]:
+    if arr is None:
+        return None
+
+    tag = Tag(tag)
+    if not isinstance(arr, TaggedArray):
+        return TaggedArray(arr, tag=tag)
+
+    return TaggedArray(arr.data, tag=tag)
