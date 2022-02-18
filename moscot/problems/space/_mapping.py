17c1997e1d7e20bb6d464795088f526e91594b7a
Zoe <zoe.piran@mail.huji.ac.il>
2022-02-19 01:49:40 +0200

    fix _mapping given mike's review

--- moscot/problems/space/_mapping.py
+++ moscot/problems/space/_mapping.py
@@ -29,13 +29,15 @@ class SpatialMappingProblem(GeneralProblem):
         solver_jit: Optional[bool] = None,
     ):
         # save full adatas for later analysis
+        
         self._adata_sc = adata_sc
         self._adata_sp = adata_sp
-
-        adata_sc_ref, adata_sp_ref = self.filter_vars(adata_sc, adata_sp, var_names, use_reference)
-        solver = FGWSolver(rank=rank, jit=solver_jit) if use_reference else GWSolver(rank=rank, jit=solver_jit)
-        super().__init__(adata_sc_ref, adata_sp_ref, solver=solver)
-        self.use_reference = use_reference
+        self._var_names = var_names
+        self._use_reference = use_reference
+        self._rank = rank
+        self._solver_jit = solver_jit
+        
+        super().__init__(adata_sc, adata_sp)
 
     @property
     def adata_sp(
@@ -47,11 +49,8 @@ class SpatialMappingProblem(GeneralProblem):
     def adata_sc(self) -> AnnData:
         return self._adata_sc
 
-    @property
-    def problems(self) -> GeneralProblem:
-        return self._problems
 
-    def filter_vars(
+    def _filter_vars(
         self,
         adata_sc: AnnData,
         adata_sp: AnnData,
@@ -81,21 +80,29 @@ class SpatialMappingProblem(GeneralProblem):
         attr_sc: Mapping[str, Any] = {"attr": "obsm", "key": "X_scvi"},
         attr_sp: Optional[Mapping[str, Any]] = {"attr": "obsm", "key": "spatial"},
         attr_joint: Optional[Mapping[str, Any]] = {"x_attr": "X", "y_attr": "X"},
+        var_names: List[str] | bool | None = None,
+        use_reference: bool = None,
         **kwargs: Any,
     ) -> GeneralProblem:
-
-        if self.use_reference:
+        # todo: decide on proper manner to init solver (pass rank\solver_jit)
+        var_names = var_names if var_names is not None else self._var_names
+        use_reference = use_reference if use_reference is not None else self._use_reference
+        
+        self._use_reference = use_reference
+        self._var_names = var_names
+        
+        rank = kwargs.pop("rank", None)
+        solver_jit = kwargs.pop("solver_jit", None)
+        rank = rank if rank is not None else self._rank
+        solver_jit = solver_jit if solver_jit is not None else self._solver_jit
+        
+        solver = FGWSolver(rank=rank, jit=solver_jit) if use_reference else GWSolver(rank=rank, jit=solver_jit)
+        
+        adata_sc, adata_sp = self._filter_vars(self._adata_sc, self._adata_sp, var_names, use_reference)
+        super().__init__(adata_sc, adata_sp, solver=solver)
+        
+        if self._use_reference:
             return super().prepare(x=attr_sc, y=attr_sp, xy=attr_joint, **kwargs)
         else:
             return super().prepare(x=attr_sc, y=attr_sp, **kwargs)
 
-    def solve(
-        self,
-        eps: Optional[float] = None,
-        alpha: float = 0.5,
-        tau_a: Optional[float] = 1.0,
-        tau_b: Optional[float] = 1.0,
-        **kwargs: Any,
-    ) -> GeneralProblem:
-
-        return super().solve(eps=eps, alpha=alpha, tau_a=tau_a, tau_b=tau_b, **kwargs)
