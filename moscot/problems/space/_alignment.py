4baa917fb7e03547ab6aafceebd681e44960955f
giovp <giov.pll@gmail.com>
2022-02-22 13:48:38 +0100

    fixes

--- moscot/problems/space/_alignment.py
+++ moscot/problems/space/_alignment.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from typing import Any, List, Tuple, Mapping, Optional
+from typing import Any, Mapping, Optional
 
 try:
     pass
@@ -9,11 +9,10 @@ except ImportError:
 
 from typing import Optional
 
-from scanpy import logging as logg
 
 from anndata import AnnData
 
-from moscot.backends.ott import GWSolver, FGWSolver
+from moscot.backends.ott import FGWSolver
 from moscot.problems._base_problem import GeneralProblem
 from moscot.problems._compound_problem import SingleCompoundProblem
 
@@ -43,18 +42,18 @@ class SpatialAlignmentProblem(SingleCompoundProblem):
         **kwargs: Any,
     ) -> GeneralProblem:
 
-        x={"attr": "obsm", "key": f"{spatial_key}"}
-        y={"attr": "obsm", "key": f"{spatial_key}"}
+        x = {"attr": "obsm", "key": f"{spatial_key}"}
+        y = {"attr": "obsm", "key": f"{spatial_key}"}
 
         super().prepare(x=x, y=y, xy=attr_joint, policy=policy, **kwargs)
 
     def solve(
         self,
-        eps: Optional[float] = None,
+        epsilon: Optional[float] = None,
         alpha: float = 0.5,
         tau_a: Optional[float] = 1.0,
         tau_b: Optional[float] = 1.0,
         **kwargs: Any,
     ) -> GeneralProblem:
 
-        super().solve(eps=eps, alpha=alpha, tau_a=tau_a, tau_b=tau_b, **kwargs)
+        return super().solve(epsilon=epsilon, alpha=alpha, tau_a=tau_a, tau_b=tau_b, **kwargs)
