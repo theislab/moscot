e59aac6ef37076d5b2f8025a8a6f6a58a2b64cf2
Michal Klein <michal.klein@protonmail.com>
2022-03-29 16:43:28 +0200

    Update simple run test

--- tests/test_general_problem.py
+++ tests/test_general_problem.py
@@ -12,9 +12,11 @@ from moscot.solvers._base_solver import BaseSolver
 
 class TestGeneralProblem:
     @pytest.mark.parametrize("solver_t", [SinkhornSolver, GWSolver, FGWSolver])
-    def test_simple_run(self, adata_x: AnnData, adata_y: AnnData, adata_xy: AnnData, solver_t: Type[BaseSolver]):
-        prob = GeneralProblem(adata_x, adata_y, adata_xy=adata_xy, solver=solver_t())
-        prob = prob.prepare(x={"attr": "X"}, y={"attr": "X"}, xy={"attr": "X", "tag": "cost"}).solve(epsilon=5e-1)
+    def test_simple_run(self, adata_x: AnnData, adata_y: AnnData, solver_t: Type[BaseSolver]):
+        prob = GeneralProblem(adata_x, adata_y, solver=solver_t())
+        prob = prob.prepare(
+            x={"attr": "X"}, y={"attr": "X"}, xy={"x_attr": "obsm", "x_key": "X_pc", "y_attr": "obsm", "y_key": "X_pc"}
+        ).solve(epsilon=5e-1)
         sol = prob.solution
 
         assert isinstance(sol, BaseSolverOutput)
