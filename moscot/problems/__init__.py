be0bd8fb667151dc781a84a8a49e8424a5e106d0
Dominik Klein <51911519+MUCDK@users.noreply.github.com>
2022-04-07 15:39:48 +0200

    Update __init__.py

--- moscot/problems/__init__.py
+++ moscot/problems/__init__.py
@@ -1,4 +1,4 @@
 from moscot.problems._base_problem import GeneralProblem
 from moscot.problems._compound_problem import CompoundProblem, MultiCompoundProblem, SingleCompoundProblem
 from moscot.problems._multimarginal_problem import MultiMarginalProblem
-from moscot.problems.time._lineage import TemporalProblem, LineageProblem
+
