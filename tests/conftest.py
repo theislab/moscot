f82416337280fe5aa7e0b28859e68eb0b045f088
Dominik Klein <domin.klein@gmail.com>
2022-04-14 09:54:53 +0200

    reformatted files

--- tests/conftest.py
+++ tests/conftest.py
@@ -11,13 +11,11 @@ from anndata import AnnData
 import scanpy as sc
 
 config.update("jax_enable_x64", True)
+from _utils import _get_random_trees
 from sklearn.metrics import pairwise_distances
 
 from jax import numpy as jnp  # noqa: E402
 import numpy as np  # noqa: E402
-
-from _utils import _get_random_trees
-
 
 Geom_t = Union[jnp.ndarray, Tuple[jnp.ndarray, jnp.ndarray]]
 RTOL = 1e-6
@@ -140,7 +138,9 @@ def adata_time_barcodes(adata_time: AnnData) -> AnnData:
 
 @pytest.fixture()
 def adata_time_trees(adata_time: AnnData) -> AnnData:
-    trees = _get_random_trees(n_leaves = 96, n_trees = 3, leaf_names=[list(adata_time[adata_time.obs.time==i].obs.index) for i in range(3)])
+    trees = _get_random_trees(
+        n_leaves=96, n_trees=3, leaf_names=[list(adata_time[adata_time.obs.time == i].obs.index) for i in range(3)]
+    )
     adata_time.uns["trees"] = {0: trees[0], 1: trees[1], 2: trees[2]}
     return adata_time
 
