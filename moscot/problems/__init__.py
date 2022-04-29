ccebceee8d29e94852f2d22d4ca8b456ba20ce58
Klein <dominik.klein@MacBook-Pro.local>
2022-04-29 11:52:35 +0200

    add CompoundBaseProblem to moscot.problems

--- moscot/problems/__init__.py
+++ moscot/problems/__init__.py
@@ -1,5 +1,5 @@
 from moscot.problems._base_problem import GeneralProblem, BaseProblem
 from moscot.problems.generic._generic import GWProblem, FGWProblem, SinkhornProblem
-from moscot.problems._compound_problem import CompoundProblem, MultiCompoundProblem, SingleCompoundProblem
+from moscot.problems._compound_problem import CompoundProblem, MultiCompoundProblem, SingleCompoundProblem, CompoundBaseProblem
 from moscot.problems._multimarginal_problem import MultiMarginalProblem
 from moscot.problems.time._lineage import TemporalBaseProblem
