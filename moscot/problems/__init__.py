2fcc8904a13ec047d97e8c75a86f9ce519bad60f
Klein <dominik.klein@MacBook-Pro.local>
2022-04-29 12:17:35 +0200

    bugfix

--- moscot/problems/__init__.py
+++ moscot/problems/__init__.py
@@ -1,5 +1,4 @@
 from moscot.problems._base_problem import BaseProblem, GeneralProblem
-from moscot.problems.time._lineage import TemporalBaseProblem
 from moscot.problems.generic._generic import GWProblem, FGWProblem, SinkhornProblem
 from moscot.problems._compound_problem import (
     CompoundProblem,
@@ -8,3 +7,4 @@ from moscot.problems._compound_problem import (
     SingleCompoundProblem,
 )
 from moscot.problems._multimarginal_problem import MultiMarginalProblem
+
