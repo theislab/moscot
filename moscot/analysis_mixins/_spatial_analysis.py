c7d9c545f1493d82df3f6791551038659de7f8d6
giovp <giov.pll@gmail.com>
2022-05-26 19:29:43 +0200

    fix mypy spatial analysis

--- moscot/analysis_mixins/_spatial_analysis.py
+++ moscot/analysis_mixins/_spatial_analysis.py
@@ -1,4 +1,4 @@
-from typing import Any, Dict, List, Tuple, Union, Mapping, Optional, Sequence
+from typing import Any, Dict, List, Tuple, Union, Mapping, Callable, Optional, Sequence
 
 from scipy.stats import pearsonr, spearmanr
 from scipy.linalg import svd
@@ -8,46 +8,65 @@ from scipy.sparse.linalg import LinearOperator
 import pandas as pd
 
 import numpy as np
-import numpy.typing as npt
 
 from anndata import AnnData
 
+from moscot._types import ArrayLike
 from moscot.problems._subset_policy import StarPolicy
-from moscot.analysis_mixins._base_analysis import AnalysisMixin
+from moscot.problems._compound_problem import K
+from moscot.analysis_mixins._base_analysis import AnalysisMixin, AnalysisMixinProtocol
 
 
-class SpatialAlignmentAnalysisMixin(AnalysisMixin):
+# AnalysisMixin_t = TypeVar("AnalysisMixin_t",bound=AnalysisMixin)
+class SpatialAnalysisMixinProtocol(AnalysisMixinProtocol[K]):
+    """Protocol class."""
+
+    adata: AnnData
+    adata_sc: AnnData
+    adata_sp: AnnData
+
+
+class SpatialAlignmentAnalysisMixin(AnalysisMixin, SpatialAnalysisMixinProtocol[K]):
     """Spatial alignment mixin class."""
 
     def __init__(self, *args: Any, **kwargs: Any) -> None:
         super().__init__(*args, **kwargs)
-        self._spatial_key: Optional[str] = None
+        _spatial_key: str
 
-    def _interpolate_scheme(self, reference: Any, mode: Literal["warp", "affine"]) -> Dict[str, npt.ArrayLike]:
+    def _interpolate_scheme(
+        self,
+        reference: Any,
+        mode: Literal["warp", "affine"],
+    ) -> Tuple[Dict[Any, ArrayLike], Optional[Dict[Any, Optional[ArrayLike]]]]:
         """Scheme for interpolation."""
 
-        def subs_adata(k: Any) -> npt.ArrayLike:
+        def subs_adata(k: Any) -> ArrayLike:
             return self.adata[self.adata.obs[self._policy._subset_key] == k].obsm[self.spatial_key].copy()
 
-        # TODO: error message for star policy
+        # TODO(giovp): error message for star policy
         # get reference
         src = subs_adata(reference)
-        transport_maps = {reference: src}
-        transport_metadata = {}
+        transport_maps: Dict[Any, ArrayLike] = {reference: src}
+        transport_metadata: Dict[Any, Optional[ArrayLike]] = {}
         if mode == "affine":
             src -= src.mean(0)
-            transport_metadata = {reference: 0}
+            transport_metadata = {reference: np.diag((1, 1))}  # 2d data
 
         # get policy
         full_steps = self._policy._subset
         fwd_steps = self._policy.plan(end=reference)
         bwd_steps = None
-        # get mapping function
-        # transport is either affine or simply mat.dot with the source spatial coords
-        _transport = self._affine if mode == "affine" else lambda tmap, _, src: (tmap.dot(src), None)
 
-        if not fwd_steps or not set(full_steps).issubset(set(fwd_steps.keys())):
-            bwd_steps = self._policy.plan(start=reference)
+        if mode == "affine":
+            _transport: Callable[
+                [LinearOperator, ArrayLike, ArrayLike], Tuple[ArrayLike, Optional[ArrayLike]]
+            ] = self._affine
+        else:
+            _transport = self._warp
+
+        if full_steps is not None:
+            if not fwd_steps or not set(full_steps).issubset(set(fwd_steps.keys())):
+                bwd_steps = self._policy.plan(start=reference)
 
         if len(fwd_steps):
             for (start, end) in fwd_steps.keys():
@@ -63,9 +82,8 @@ class SpatialAlignmentAnalysisMixin(AnalysisMixin):
             return transport_maps, transport_metadata
         return transport_maps, None
 
-    def _affine(
-        self, tmap: Union[npt.ArrayLike, LinearOperator], tgt: npt.ArrayLike, src: npt.ArrayLike
-    ) -> Tuple[npt.ArrayLike, npt.ArrayLike]:
+    @staticmethod
+    def _affine(tmap: LinearOperator, tgt: ArrayLike, src: ArrayLike) -> Tuple[ArrayLike, ArrayLike]:
         """Affine transformation."""
         tgt -= tgt.mean(0)
         H = tgt.T.dot(tmap.dot(src))
@@ -74,13 +92,18 @@ class SpatialAlignmentAnalysisMixin(AnalysisMixin):
         tgt = R.dot(tgt.T).T
         return tgt, R
 
+    @staticmethod
+    def _warp(tmap: LinearOperator, tgt: ArrayLike, src: ArrayLike) -> Tuple[ArrayLike, None]:
+        """Warp transformation."""
+        return tmap.dot(src), None
+
     def align(
         self,
         reference: Any,
         mode: Literal["warp", "affine"] = "warp",
         inplace: bool = False,
-    ) -> Optional[Union[npt.ArrayLike, Tuple[npt.ArrayLike, Dict[Any, npt.ArrayLike]]]]:
-        """Alignemnt method."""
+    ) -> Optional[Union[ArrayLike, Tuple[ArrayLike, Optional[Dict[Any, Optional[ArrayLike]]]]]]:
+        """Alignment method."""
         if reference not in self._policy._cat.categories:
             raise ValueError(f"`reference: {reference}` not in policy categories: {self._policy._cat.categories}.")
         if isinstance(self._policy, StarPolicy):
@@ -90,10 +113,10 @@ class SpatialAlignmentAnalysisMixin(AnalysisMixin):
         aligned_basis = np.vstack([aligned_maps[k] for k in self._policy._cat.categories])
 
         if mode == "affine":
-            if inplace:
+            if not inplace:
                 return aligned_basis, aligned_metadata
             self.adata.uns[self.spatial_key]["alignment_metadata"] = aligned_metadata
-        if inplace:
+        if not inplace:
             return aligned_basis
         self.adata.obsm[f"{self.spatial_key}_{mode}"] = aligned_basis
 
@@ -110,34 +133,34 @@ class SpatialAlignmentAnalysisMixin(AnalysisMixin):
         self._spatial_key = value
 
 
-class SpatialMappingAnalysisMixin(AnalysisMixin):
+class SpatialMappingAnalysisMixin(AnalysisMixin, SpatialAnalysisMixinProtocol[K]):
     """Spatial mapping analysis mixin class."""
 
     def _filter_vars(
         self,
         var_names: Optional[Sequence[Any]] = None,
     ) -> Optional[List[str]]:
-        """Filter variables for Sinkhorn tem."""
+        """Filter variables for Sinkhorn term."""
         vars_sc = set(self.adata_sc.var_names)  # TODO: allow alternative gene symbol by passing var_key
         vars_sp = set(self.adata.var_names)
-        var_names = set(var_names) if var_names is not None else None
-        if var_names is None:
-            var_names = vars_sp.intersection(vars_sc)
-            if len(var_names):
-                return list(var_names)
+        _var_names = set(var_names) if var_names is not None else None
+        if _var_names is None:
+            _var_names = vars_sp.intersection(vars_sc)
+            if len(_var_names):
+                return list(_var_names)
             raise ValueError("`adata_sc` and `adata_sp` do not share `var_names`. Input valid `var_names`.")
-        if not len(var_names):
+        if not len(_var_names):
             return None
-        if var_names.issubset(vars_sc) and var_names.issubset(vars_sp):
-            return list(var_names)
+        if _var_names.issubset(vars_sc) and _var_names.issubset(vars_sp):
+            return list(_var_names)
         raise ValueError("Some `var_names` ares missing in either `adata_sc` or `adata_sp`.")
 
     def correlate(
         self, var_names: Optional[List[str]] = None, corr_method: Literal["pearson", "spearman"] = "pearson"
-    ) -> Mapping[Tuple[str, Any], pd.Series]:
+    ) -> Mapping[Tuple[K, K], pd.Series]:
         """Calculate correlation between true and predicted gexp in space."""
         var_sc = self._filter_vars(var_names)
-        if not len(var_sc):
+        if var_sc is None or not len(var_sc):
             raise ValueError("No overlapping `var_names` between ` adata_sc` and `adata_sp`.")
         cor = pearsonr if corr_method == "pearson" else spearmanr
         corr_dic = {}
