b9d4c738b20e7184282c480c7d35d3f730321b8e
Dominik Klein <dominik.klein@helmholtz-munich.de>
2022-10-10 16:43:07 +0200

    add references and adapt sinkhorn initializer (#296)
    * add references and adapt sinkhorn initializer
    
    * add references to quad initializers

--- moscot/_types.py
+++ moscot/_types.py
@@ -15,6 +15,6 @@ except (ImportError, TypeError):
 Numeric_t = Union[int, float]  # type of `time_key` arguments
 Filter_t = Optional[Union[str, Mapping[str, Sequence[Any]]]]  # type how to filter adata
 Str_Dict_t = Union[str, Mapping[str, Sequence[Any]]]  # type for `cell_transition`
-SinkhornInitializer_t = Optional[Literal["GaussianInitializer", "SortingInitializer"]]
+SinkhornInitializer_t = Optional[Literal["default", "gaussian", "sorting"]]
 QuadInitializer_t = Optional[Literal["random", "rank2", "k-means", "generalized-k-means"]]
 Initializer_t = Union[SinkhornInitializer_t, QuadInitializer_t]
