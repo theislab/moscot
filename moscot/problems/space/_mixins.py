a3f6a8e562d01b14558314775bf3c7354f31a129
giovp <giov.pll@gmail.com>
2022-10-20 18:22:44 +0200

    init

--- moscot/problems/space/_mixins.py
+++ moscot/problems/space/_mixins.py
@@ -5,6 +5,9 @@ from networkx import NetworkXNoPath
 from scipy.stats import pearsonr, spearmanr
 from scipy.linalg import svd
 from scipy.sparse import issparse
+from scipy.spatial import ConvexHull
+from sklearn.metrics import pairwise_distances
+from sklearn.neighbors import NearestNeighbors
 from scipy.sparse.linalg import LinearOperator
 import pandas as pd
 
@@ -15,6 +18,7 @@ from anndata import AnnData
 from moscot._types import ArrayLike, Str_Dict_t
 from moscot._docs._docs import d
 from moscot.problems.base import AnalysisMixin  # type: ignore[attr-defined]
+from moscot._constants._key import Key
 from moscot._docs._docs_mixins import d_mixins
 from moscot.backends.ott._output import Device_t
 from moscot._constants._constants import CorrMethod, AlignmentMode, AggregationMode, PlottingDefaults
@@ -60,6 +64,8 @@ class SpatialMappingMixinProtocol(AnalysisMixinProtocol[K, B]):
     adata_sc: AnnData
     adata_sp: AnnData
     batch_key: Optional[str]
+    spatial_key: Optional[str]
+    _spatial_key: Optional[str]
 
     def _filter_vars(
         self: "SpatialMappingMixinProtocol[K, B]",
@@ -280,6 +286,7 @@ class SpatialMappingMixin(AnalysisMixin[K, B]):
     def __init__(self, *args: Any, **kwargs: Any) -> None:
         super().__init__(*args, **kwargs)
         self._batch_key: Optional[str] = None
+        self._spatial_key: Optional[str] = None
 
     def _filter_vars(
         self: SpatialMappingMixinProtocol[K, B],
@@ -379,6 +386,55 @@ class SpatialMappingMixin(AnalysisMixin[K, B]):
         adata_pred.var_names = var_names
         return adata_pred
 
+    def compute_spatial_correspondence(
+        self: SpatialMappingMixinProtocol[K, B],
+        interval: Union[ArrayLike, int] = 10,
+        max_dist: Optional[int] = None,
+        spatial_key: Union[str, Mapping[str, Any]] = Key.obsm.spatial,
+    ) -> pd.DataFrame:
+        """
+        Compute structural correspondence between spatial and molecular distances.
+
+        Parameters
+        ----------
+        interval
+            Interval for the spatial distance.
+        max_dist
+            Maximum distance for the interval, if `None` it is set from data.
+
+        Returns
+        -------
+        :class:`pandas.DataFrame` with columns:
+
+            - `spatial`: average spatial distance.
+            - `expression`: average expression distance.
+            - `index`: index of the interval.
+            - `{batch_key}`: key of the batch (slide).
+        """
+        if self.batch_key is not None:
+            out_list = []
+            categ = self.adata.obs[self.batch_key].cat.categories
+            if len(categ) > 1:
+                for c in categ:
+                    adata_subset = self.adata[self.adata.obs[self.batch_key] == c]
+                    spatial = adata_subset.obsm[self.spatial_key]
+                    gexp = adata_subset.obsm[self.spatial_key]
+                    out = _compute_correspondence(spatial, gexp, interval, max_dist)
+                    out[self.batch_key] = c
+                    out_list.append(out)
+            else:
+                spatial = self.adata.obsm[self.spatial_key]
+                gexp = self.adata.obsm[self.spatial_key]
+                out = _compute_correspondence(spatial, gexp, interval, max_dist)
+                out[self.batch_key] = categ[0]
+                out_list.append(out)
+            return pd.concat(out_list, axis=0)
+        else:
+            spatial = self.adata.obsm[self.spatial_key]
+            gexp = self.adata.obsm[self.spatial_key]
+            out = _compute_correspondence(spatial, gexp, interval, max_dist)
+            return out
+
     @d_mixins.dedent
     def cell_transition(
         self: SpatialMappingMixinProtocol[K, B],
@@ -445,3 +501,68 @@ class SpatialMappingMixin(AnalysisMixin[K, B]):
         if value is not None and value not in self.adata.obs:
             raise KeyError(f"{value} not in `adata.obs`.")
         self._batch_key = value
+
+    @property
+    def spatial_key(self) -> Optional[str]:
+        """Return spatial key."""
+        return self._spatial_key
+
+    @spatial_key.setter
+    def spatial_key(self: SpatialAlignmentMixinProtocol[K, B], value: Optional[str]) -> None:
+        if value is not None and value not in self.adata.obsm:
+            raise KeyError(f"TODO: {value} not found in `adata.obsm`.")
+        self._spatial_key = value
+
+
+def _compute_correspondence(
+    spatial: ArrayLike,
+    gexp: ArrayLike,
+    interval: Union[ArrayLike, int] = 10,
+    max_dist: Optional[int] = None,
+) -> pd.DataFrame:
+    if isinstance(interval, int):
+        # prepare support
+        spatial.shape[0]
+        hull = ConvexHull(spatial)
+        area = hull.volume
+        if max_dist is None:
+            max_dist = (area / 2) ** 0.5
+        support = np.linspace(max_dist / interval, max_dist, interval)
+    else:
+        support = np.array(sorted(interval), dtype=np.float_, copy=True)
+
+    def pdist(row_idx: ArrayLike, col_idx: float, gexp: ArrayLike) -> Any:
+        if len(row_idx) > 0:
+            return pairwise_distances(gexp[row_idx, :], gexp[[col_idx], :]).mean()  # type: ignore[index]
+
+    vmean = np.vectorize(lambda x: x.mean())
+    vpdist = np.vectorize(pdist, excluded=["gexp"])
+
+    spatial_arr = []
+    gexp_arr = []
+    index_arr = []
+
+    for ind, i in enumerate(support):
+        tree = NearestNeighbors(radius=i, metric="euclidean")
+        tree.fit(spatial)
+        dist, idx = tree.radius_neighbors()
+
+        spatial_dist = vmean(dist)
+        spatial_dist = spatial_dist[~np.isnan(spatial_dist)]
+
+        gexp_dist = vpdist(row_idx=idx, col_idx=np.arange(len(idx)), gexp=gexp)
+        gexp_dist = gexp_dist[~np.isnan(gexp_dist)]
+        assert spatial_dist.shape == gexp_dist.shape, "Distances array should be equal."
+
+        spatial_arr.append(spatial_dist)
+        gexp_arr.append(gexp_dist)
+        index_arr.append(np.repeat(ind, gexp_dist.shape[0]))
+
+    spatial_arr = np.concatenate(spatial_arr)
+    gexp_arr = np.concatenate(gexp_arr)
+    index_arr = np.concatenate(index_arr)
+
+    df = pd.DataFrame(np.vstack([spatial_arr, gexp_arr, index_arr]).T, columns=["spatial", "expression", "index"])
+
+    df["index"] = pd.Categorical(df["index"].astype(np.int_))
+    return df
