10205ad1de10110f9258b23e0b8214b6d16674b9
Dominik Klein <dominik.klein@helmholtz-munich.de>
2022-10-23 18:05:58 +0200

    fix import

--- tests/problems/spatio_temporal/test_spatio_temporal_problem.py
+++ tests/problems/spatio_temporal/test_spatio_temporal_problem.py
@@ -11,10 +11,10 @@ from tests.problems.conftest import (
     fgw_args_1,
     fgw_args_2,
     geometry_args,
+    gw_solver_args,
     quad_prob_args,
-    fgw_solver_args,
     pointcloud_args,
-    fgw_sinkhorn_solver_args,
+    gw_sinkhorn_solver_args,
 )
 from moscot.problems.time._lineage import BirthDeathProblem
 from moscot.problems.spatio_temporal import SpatioTemporalProblem
@@ -113,12 +113,12 @@ class TestSpatioTemporalProblem:
 
         key = (0, 1)
         solver = problem[key]._solver._solver
-        for arg, val in fgw_solver_args.items():
+        for arg, val in gw_solver_args.items():
             assert hasattr(solver, val)
             assert getattr(solver, val) == args_to_check[arg]
 
         sinkhorn_solver = solver.linear_ot_solver
-        for arg, val in fgw_sinkhorn_solver_args.items():
+        for arg, val in gw_sinkhorn_solver_args.items():
             assert hasattr(sinkhorn_solver, val)
             assert getattr(sinkhorn_solver, val) == args_to_check[arg]
 
