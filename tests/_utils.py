043062512b73a4eb3e994cbdf5c5599ebe680163
Dominik Klein <dominik.klein@helmholtz-munich.de>
2022-10-28 11:12:29 +0200

    adapt tree costs

--- tests/_utils.py
+++ tests/_utils.py
@@ -1,7 +1,6 @@
 from typing import Any, List, Type, Tuple, Union, Optional
 
 from scipy.sparse import csr_matrix
-import networkx as nx
 
 import numpy as np
 
@@ -50,30 +49,6 @@ class MockBaseSolverOutput:
         rng = np.random.RandomState(42)
         self.a = rng.randn(len_a)
         self.b = rng.randn(len_b)
-
-
-def _get_random_trees(
-    n_leaves: int, n_trees: int, n_initial_nodes: int = 50, leaf_names: Optional[List[List[str]]] = None, seed: int = 42
-) -> Tuple[nx.DiGraph, ...]:
-    rng = np.random.RandomState(42)
-    if leaf_names is not None:
-        assert len(leaf_names) == n_trees
-    trees = []
-    for tree_idx in range(n_trees):
-        G = nx.random_tree(n_initial_nodes, seed=seed, create_using=nx.DiGraph)
-        leaves = [x for x in G.nodes() if G.out_degree(x) == 0 and G.in_degree(x) == 1]
-        inner_nodes = list(set(G.nodes()) - set(leaves))
-        leaves_updated = leaves
-        for i in range(n_leaves - len(leaves)):
-            G.add_node(n_initial_nodes + i)
-            G.add_edge(rng.choice(inner_nodes, 1)[0], n_initial_nodes + i)
-            leaves_updated.append(n_initial_nodes + i)
-        assert len(leaves_updated) == n_leaves
-        relabel_dict = {leaves_updated[i]: leaf_names[tree_idx][i] for i in range(len(leaves_updated))}
-        G = nx.relabel_nodes(G, relabel_dict)
-        trees.append(G)
-
-    return trees
 
 
 def _make_adata(grid: ArrayLike, n: int, seed) -> List[AnnData]:
