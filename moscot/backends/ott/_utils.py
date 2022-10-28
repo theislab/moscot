bcfcaf80b5b2e8104a79c75b44644c3a65987710
Michal Klein <michal.klein@protonmail.com>
2022-10-28 17:24:48 +0200

    Update sinkhorn divergence wrapper

--- moscot/backends/ott/_utils.py
+++ moscot/backends/ott/_utils.py
@@ -2,6 +2,7 @@ from typing import Any, Optional
 
 from ott.geometry.pointcloud import PointCloud
 from ott.tools.sinkhorn_divergence import sinkhorn_divergence
+import jax.numpy as jnp
 
 from moscot._types import ArrayLike, ScaleCost_t
 from moscot._logging import logger
@@ -12,17 +13,25 @@ def _compute_sinkhorn_divergence(
     point_cloud_2: ArrayLike,
     a: Optional[ArrayLike] = None,
     b: Optional[ArrayLike] = None,
-    epsilon: float = 10,
-    scale_cost: ScaleCost_t = 1,
+    epsilon: float = 10.0,
+    scale_cost: ScaleCost_t = 1.0,
     **kwargs: Any,
 ) -> float:
+    point_cloud_1 = jnp.asarray(point_cloud_1)
+    point_cloud_2 = jnp.asarray(point_cloud_2)
+    a = None if a is None else jnp.asarray(a)
+    b = None if b is None else jnp.asarray(b)
+
     output = sinkhorn_divergence(
         PointCloud, x=point_cloud_1, y=point_cloud_2, a=a, b=b, epsilon=epsilon, scale_cost=scale_cost, **kwargs
     )
-    if not output.converged[0]:
-        logger.warning("TODO: Solver not converged in x to y term.")
-    if not output.converged[1]:
-        logger.warning("TODO: Solver not converged in x to x term.")
-    if len(output.converged) > 1 and not output.converged[2]:
-        logger.warning("TODO: Solver not converged in y to y term.")
+    xy_conv, xx_conv, *yy_conv = output.converged
+
+    if not xy_conv:
+        logger.warning("Solver did not converged in the `x/y` term.")
+    if not xx_conv:
+        logger.warning("Solver did not converged in the `x/x` term.")
+    if len(yy_conv) and not yy_conv[0].converged:
+        logger.warning("Solver did not converged in the `y/y` term.")
+
     return float(output.divergence)
