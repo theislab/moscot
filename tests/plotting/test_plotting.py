c8776151f3102893fc03236179ce0340d644caac
Giovanni Palla <25887487+giovp@users.noreply.github.com>
2022-10-30 16:46:14 +0100

    add plotting tests (#370)

--- tests/plotting/test_plotting.py
+++ tests/plotting/test_plotting.py
@@ -1,43 +1,35 @@
-from typing import List, Optional
-
-import pytest
-
-import numpy as np
-
 from anndata import AnnData
+import scanpy as sc
 
-from tests._utils import Problem
-from moscot.plotting._utils import _input_to_adatas
+from tests.plotting.conftest import PlotTester, PlotTesterMeta
 import moscot.plotting as mpl
 
+sc.pl.set_rcParams_defaults()
+sc.set_figure_params(dpi=40, color_map="viridis")
 
-class TestMoscotPl:
-    def test_input_to_adatas_problem(self, adata_time: AnnData):
-        p = Problem(adata_time)
-        adata1, adata2 = _input_to_adatas(p)
-        assert isinstance(adata1, AnnData)
-        assert isinstance(adata2, AnnData)
-        np.testing.assert_array_equal(adata1.X.A, adata_time.X.A)
-        np.testing.assert_array_equal(adata2.X.A, adata_time.X.A)
+# WARNING:
+# 1. all classes must both subclass PlotTester and use metaclass=PlotTesterMeta
+# 2. tests which produce a plot must be prefixed with `test_plot_`
+# 3. if the tolerance needs to be changed, don't prefix the function with `test_plot_`, but with something else
+#    the comp. function can be accessed as `self.compare(<your_filename>, tolerance=<your_tolerance>)`
+#    ".png" is appended to <your_filename>, no need to set it
 
-    def test_input_to_adatas_adata(self, adata_time: AnnData):
-        adata1, adata2 = _input_to_adatas(adata_time)
-        assert isinstance(adata1, AnnData)
-        assert isinstance(adata2, AnnData)
-        np.testing.assert_array_equal(adata1.X.A, adata_time.X.A)
-        np.testing.assert_array_equal(adata2.X.A, adata_time.X.A)
 
-    def test_cell_transition(self, adata_pl_cell_transition: AnnData):
-        mpl.cell_transition(adata_pl_cell_transition)
+class TestPlotting(PlotTester, metaclass=PlotTesterMeta):
+    def test_plot_cell_transition(self, adata_pl_cell_transition: AnnData):
         mpl.cell_transition(adata_pl_cell_transition)
 
-    @pytest.mark.parametrize("time_points", [None, [0]])
-    def test_push(self, adata_pl_push: AnnData, time_points: Optional[List[int]]):
-        _ = mpl.push(adata_pl_push, time_points=time_points)
+    def test_plot_cell_transition_params(self, adata_pl_cell_transition: AnnData):
+        mpl.cell_transition(adata_pl_cell_transition, annotate=None, cmap="inferno", fontsize=15)
 
-    @pytest.mark.parametrize("time_points", [None, [0]])
-    def test_pull(self, adata_pl_pull: AnnData, time_points: Optional[List[int]]):
-        _ = mpl.pull(adata_pl_pull, time_points=time_points)
-
-    def test_sankey(self, adata_pl_sankey: AnnData):
+    def test_plot_sankey(self, adata_pl_sankey: AnnData):
         mpl.sankey(adata_pl_sankey)
+
+    def test_plot_sankey_params(self, adata_pl_sankey: AnnData):
+        mpl.sankey(adata_pl_sankey, captions=["Test", "Other test"], titel="Title", figsize=(3, 3))
+
+    def test_plot_push(self, adata_pl_push: AnnData):
+        mpl.push(adata_pl_push, time_points=[2])
+
+    def test_plot_pull(self, adata_pl_pull: AnnData):
+        mpl.pull(adata_pl_pull, time_points=[1])
