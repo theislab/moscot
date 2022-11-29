0e42fbc98d8411c4af7563453cd74a82b3495162
lucaeyring <luca.eyring@googlemail.com>
2022-11-29 16:15:15 +0100

    fix for data sampler and growth rates

--- src/moscot/backends/ott/_jax_data.py
+++ src/moscot/backends/ott/_jax_data.py
@@ -40,8 +40,42 @@ class JaxSampler:
             """Jitted sample function."""
             return jax.random.choice(key, self.data_target, shape=[self.batch_size], p=self.b)
 
+        @jax.jit
+        def _compute_unbalanced_marginals(
+            batch_source: jnp.ndarray,
+            batch_target: jnp.ndarray,
+        ) -> Tuple[jnp.ndarray, jnp.ndarray]:
+            """Jitted function to compute the source and target marginals for a batch."""
+            geom = PointCloud(batch_source, batch_target, epsilon=self.epsilon, scale_cost="mean")
+            out = sinkhorn(
+                geom,
+                tau_a=self.tau_a,
+                tau_b=self.tau_b,
+                jit=False,
+                max_iterations=1e7,
+            )
+            # get flattened log transition matrix
+            transition_matrix = geom.transport_from_potentials(out.f, out.g)
+            # jax categorical uses log probabilities
+            log_marginals_source = jnp.log(jnp.sum(transition_matrix, axis=1))
+            log_marginals_target = jnp.log(jnp.sum(transition_matrix, axis=0))
+            return log_marginals_source, log_marginals_target
+
+        @jax.jit
+        def _unbalanced_resample(
+            key: jax.random.KeyArray,
+            batch: jnp.ndarray,
+            log_marginals: jnp.ndarray,
+        ) -> jnp.ndarray:
+            """Resample a batch based upon log marginals."""
+            # sample from marginals
+            indices = jax.random.categorical(key, log_marginals, shape=[self.batch_size])
+            return batch[indices]
+
         self._sample_source = _sample_source
         self._sample_target = _sample_target
+        self.compute_unbalanced_marginals = _compute_unbalanced_marginals
+        self.unbalanced_resample = _unbalanced_resample
 
     def __call__(
         self,
@@ -57,37 +91,3 @@ class JaxSampler:
         if source:
             return self._sample_source(key)
         return self._sample_target(key)
-
-    @jax.jit
-    def compute_unbalanced_marginals(
-        self,
-        batch_source: jnp.ndarray,
-        batch_target: jnp.ndarray,
-    ) -> jnp.ndarray:
-        """Jitted function to compute the source and target marginals for a batch."""
-        geom = PointCloud(batch_source, batch_target, epsilon=self.epsilon, scale_cost="mean")
-        out = sinkhorn(
-            geom,
-            tau_a=self.tau_a,
-            tau_b=self.tau_b,
-            jit=False,
-            max_iterations=1e7,
-        )
-        # get flattened log transition matrix
-        transition_matrix = geom.transport_from_potentials(out.f, out.g)
-        # jax categorical uses log probabilities
-        log_marginals_source = jnp.log(jnp.sum(transition_matrix, axis=1))
-        log_marginals_target = jnp.log(jnp.sum(transition_matrix, axis=0))
-        return log_marginals_source, log_marginals_target
-
-    @jax.jit
-    def unbalanced_resample(
-        self,
-        key: jax.random.KeyArray,
-        batch: jnp.ndarray,
-        log_marginals: jnp.ndarray,
-    ) -> jnp.ndarray:
-        """Resample a batch based upon log marginals."""
-        # sample from marginals
-        indices = jax.random.categorical(key, log_marginals, shape=[self.batch_size])
-        return batch[indices]
