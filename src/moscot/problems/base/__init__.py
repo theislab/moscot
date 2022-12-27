51eb514e94ea47df406d896dfe81201ac06cc8dc
Dominik Klein <dominik.klein@helmholtz-munich.de>
2022-12-27 10:07:44 +0100

    [ci skip] add generic neural ot classes

--- src/moscot/problems/base/__init__.py
+++ src/moscot/problems/base/__init__.py
@@ -1,4 +1,4 @@
 from moscot.problems.base._mixins import AnalysisMixin
 from moscot.problems.base._birth_death import BirthDeathMixin, BirthDeathProblem
-from moscot.problems.base._base_problem import OTProblem, BaseProblem, NeuralOTProblem
+from moscot.problems.base._base_problem import OTProblem, BaseProblem, CondOTProblem, NeuralOTProblem
 from moscot.problems.base._compound_problem import CompoundProblem, BaseCompoundProblem
