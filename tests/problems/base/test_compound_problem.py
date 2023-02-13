1b3214e9a02c797c49fbd52ece5336679dd5fec5
michalk8 <46717574+michalk8@users.noreply.github.com>
2023-02-13 15:01:03 +0100

    Feature/convergence plotting (#470)
    * Remove `plot_convergence`, add `plot_{costs,errors}`
    
    * Add tests
    
    * Fix tests

--- tests/problems/base/test_compound_problem.py
+++ tests/problems/base/test_compound_problem.py
@@ -7,7 +7,7 @@ import pytest
 
 from ott.geometry.costs import Cosine, Euclidean, SqEuclidean
 from ott.geometry.pointcloud import PointCloud
-from ott.solvers.linear.sinkhorn import sinkhorn
+from ott.solvers.linear.sinkhorn import solve as sinkhorn
 import numpy as np
 
 from anndata import AnnData
