fix to projected tm computation

src/moscot/backends/ott/_output.py
@@ -219,7 +219,9 @@ class NeuralOutput(BaseSolverOutput):
             distances /= jnp.sum(distances, axis=1)[:, None]
             distances_list.append(distances.flatten())
             column_indices.append(indices.flatten())
-            row_indices.append(jnp.repeat(jnp.arange(index, index + min(batch_size, len(src))), min(k, len(tgt))))
+            row_indices.append(
+                jnp.repeat(jnp.arange(index, index + min(batch_size, len(src) - index)), min(k, len(tgt)))
+            )
         # create sparse matrix with normalized exp(-d(x,y)) as entries
         distances = jnp.concatenate(distances_list)
         row_indices = jnp.concatenate(row_indices)

src/moscot/backends/ott/_utils.py
@@ -63,7 +63,7 @@ def get_nearest_neighbors(
     input_batch: jnp.ndarray, target: jnp.ndarray, k: int = 30
 ) -> Tuple[jnp.ndarray, jnp.ndarray]:
     """Get the k nearest neighbors of the input batch in the target."""
-    k = max(input_batch.shape[0], k)
+    k = min(input_batch.shape[0], k)
     pairwise_euclidean_distances = -1 * jnp.sqrt(jnp.sum((input_batch - target) ** 2, axis=-1))
     distances, indices = jax.lax.top_k(pairwise_euclidean_distances, k=k)
     return distances, indices