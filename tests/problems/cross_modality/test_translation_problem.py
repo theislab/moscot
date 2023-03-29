0ec5238779c7a50651a67b4c15c8d6273137f9c7
nosander <nina.sander@tum.de>
2023-03-29 15:25:44 +0200

    initial test

--- tests/problems/cross_modality/test_translation_problem.py
+++ tests/problems/cross_modality/test_translation_problem.py
@@ -33,23 +33,19 @@ class TestTranslationProblem:
         joint_attr: Optional[Mapping[str, str]]
         ):
 
-        adata_src, adata_tgt = _adata_modality_split(adata_space_rotate)
-        expected_keys = ("src", "tgt") # {(i, "ref") for i in adata_src.obs.batch.cat.categories}
-        n_obs = adata_tgt.shape[0]
-        x_n_var = adata_src.obsm["spatial"].shape[1]
-        y_n_var = adata_tgt.shape[1] if sc_attr["attr"] == "X" else adata_tgt.obsm["X_pca"].shape[1]
-        xy_n_vars = adata_src.X.shape[1] if joint_attr == "default" else adata_tgt.obsm["X_pca"].shape[1]
+        adata_src, adata_tgt = _adata_modality_split(adata_space_rotate) 
+        n_obs_src = adata_src.shape[0]        
+        n_obs_tgt = adata_tgt.shape[0]
         
         tp = TranslationProblem(adata_src, adata_tgt)
         assert tp.problems == {}
         assert tp.solutions == {}
 
+        # test dummy policy
+        prob_key = ("src", "tgt")
         tp = tp.prepare(src_attr = src_attr, tgt_attr = tgt_attr, joint_attr=joint_attr)
 
-        assert len(tp) == len(expected_keys)
-        for prob_key in expected_keys:
-            assert isinstance(tp[prob_key], tp._base_problem_type)
-            assert tp[prob_key].shape == (n_obs, n_obs)
-            assert tp[prob_key].x.data_src.shape == (n_obs, x_n_var)
-            assert tp[prob_key].y.data_src.shape == (n_obs, y_n_var)
-            assert tp[prob_key].xy.data_src.shape == tp[prob_key].xy.data_tgt.shape == (n_obs, xy_n_vars)
+        assert len(tp) == 1
+        assert isinstance(tp[prob_key], tp._base_problem_type)
+        assert tp[prob_key].shape == (n_obs_src, n_obs_tgt)
+        np.testing.assert_array_equal(tp._policy._cat, prob_key)
