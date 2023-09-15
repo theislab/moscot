17d48b0774eb40088028cbe39a9f7e4d6f257da1
Arina Danilina <danilina@cip.ifi.lmu.de>
2023-09-15 18:29:09 +0200

    anno_mapping changes

--- src/moscot/base/problems/_mixins.py
+++ src/moscot/base/problems/_mixins.py
@@ -300,26 +300,31 @@ class AnalysisMixin(Generic[K, B]):
     def _annotation_mapping(
         self: AnalysisMixinProtocol[K, B],
         mapping_mode: Literal["sum", "max"],
-        source_groups: Str_Dict_t,
-        target_groups: Str_Dict_t,
+        #source_groups: Str_Dict_t,
+        #target_groups: Str_Dict_t,
+        label: str,
+        forward: bool,
         other_adata: Optional[str] = None,
         scale_by_marginals: bool = True,
         cell_transition_kwargs: Mapping[str, Any] = types.MappingProxyType({}),
     ) -> pd.DataFrame:
         if mapping_mode == "sum":
-            return self._cell_transition(**cell_transition_kwargs)
+            return self._cell_transition(**cell_transition_kwargs) #aggregation mode should set to cell
         if mapping_mode == "max":
             assert (
                 not cell_transition_kwargs
             ), "cell_transition_kwargs is not empty, although cell_transition is not used."
-            source_annotation_key, source_annotations, source_annotations_ordered = _validate_args_cell_transition(
-                self.adata, source_groups
-            )
-            target_annotation_key, target_annotations, target_annotations_ordered = _validate_args_cell_transition(
-                self.adata if other_adata is None else other_adata, target_groups
-            )
+            if forward:
+                source_annotation_key, source_annotations, source_annotations_ordered = _validate_args_cell_transition(
+                    self.adata, label
+                )
+            elif not forward:
+                target_annotation_key, target_annotations, target_annotations_ordered = _validate_args_cell_transition(
+                    self.adata if other_adata is None else other_adata, label
+                )
             dummy = pd.get_dummies(source_annotations)
             out: ArrayLike = self.pull(dummy, scale_by_marginals=scale_by_marginals)  # or assert out is not None
+            # 
             return pd.Categorical([dummy.columns[i] for i in np.array(out.argmax(1))])
         raise NotImplementedError(f"Mapping mode `{mapping_mode!r}` is not yet implemented.")
 
