d657ea4e0cc3cbbe2b818fa86b213956eb18e475
giovp <giov.pll@gmail.com>
2023-12-20 11:36:22 +0100

    update

--- src/moscot/backends/ott/solver.py
+++ src/moscot/backends/ott/solver.py
@@ -102,7 +102,10 @@ class OTTJaxSolver(OTSolver[OTTOutput], abc.ABC):
                     n_src, n_tgt = n_src_tgt
                     if n_src + n_tgt != arr.shape[0]:
                         raise ValueError(f"Expected `x` to have `{n_src + n_tgt}` points, found `{arr.shape[0]}`.")
-                    arr = arr[:n_src, n_src:]
+                    cm = geodesic.Geodesic.from_graph(arr, t=epsilon, directed=True, **kwargs).cost_matrix[
+                        :n_src, n_src:
+                    ]
+                    return geometry.Geometry(cm)
                 return geodesic.Geodesic.from_graph(arr, t=epsilon, directed=True, **kwargs)
             raise NotImplementedError(f"If the geometry is a graph, `cost` must be `geodesic`, found `{x.cost}`.")
         raise NotImplementedError(f"Creating geometry from `tag={x.tag!r}` is not yet implemented.")
