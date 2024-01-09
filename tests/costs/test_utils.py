924c78a124b48b3a791a2d188d1ba3aa112535ba
Selman Özleyen <32667648+selmanozleyen@users.noreply.github.com>
2024-01-09 15:55:42 +0100

    Cost Tests (#637)
    * added tests for cost utils
    
    * add barcode distance tests
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * fix naming
    
    * refactor for precommit
    
    * added tests for leaf_distance
    
    * fix docstring formatting
    
    * fixed the tests
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: Dominik Klein <dominik.klein@helmoltz-munich.de>
    Co-authored-by: Dominik Klein <dominik.klein@helmholtz-munich.de>

---
+++ tests/costs/test_utils.py
@@ -0,0 +1,52 @@
+import pytest
+
+from moscot.costs._utils import (
+    _get_available_backends_and_costs,
+    get_available_costs,
+    get_cost,
+)
+
+
+class TestCostUtils:
+    ALL_BACKENDS_N_COSTS = {
+        "moscot": ("barcode_distance", "leaf_distance"),
+        "ott": (
+            "euclidean",
+            "sq_euclidean",
+            "cosine",
+            "pnorm_p",
+            "sq_pnorm",
+            "elastic_l1",
+            "elastic_l2",
+            "elastic_stvs",
+            "elastic_sqk_overlap",
+        ),
+    }
+
+    @staticmethod
+    def test_get_available_backends_n_costs():
+        assert dict(_get_available_backends_and_costs()) == {
+            k: list(v) for k, v in _get_available_backends_and_costs().items()
+        }
+
+    @staticmethod
+    def test_get_available_costs():
+        assert get_available_costs() == TestCostUtils.ALL_BACKENDS_N_COSTS
+        assert get_available_costs("moscot") == {"moscot": (TestCostUtils.ALL_BACKENDS_N_COSTS["moscot"])}
+        assert get_available_costs("ott") == {"ott": TestCostUtils.ALL_BACKENDS_N_COSTS["ott"]}
+        with pytest.raises(KeyError):
+            get_available_costs("foo")
+
+    @staticmethod
+    def test_get_cost_fails():
+        invalid_cost = "foo"
+        invalid_backend = "bar"
+        with pytest.raises(
+            ValueError, match=f"Cost `{invalid_cost!r}` is not available for backend `{invalid_backend!r}`."
+        ):
+            get_cost(invalid_cost, backend=invalid_backend)
+        for backend in TestCostUtils.ALL_BACKENDS_N_COSTS:
+            with pytest.raises(
+                ValueError, match=f"Cost `{invalid_cost!r}` is not available for backend `{backend!r}`."
+            ):
+                get_cost(invalid_cost, backend=backend)
