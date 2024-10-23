e4dea94ecc742980b1b5afb28f6a2b0559d2d21d
selmanozleyen <syozleyen@gmail.com>
2024-10-23 15:54:47 +0200

    adapt tests for solvers new api

--- tests/problems/generic/test_fgw_problem.py
+++ tests/problems/generic/test_fgw_problem.py
@@ -114,7 +114,7 @@ class TestFGWProblem:
         for arg, val in args.items():
             assert getattr(solver, val, object()) == args_to_check[arg], arg
 
-        sinkhorn_solver = solver.linear_ot_solver if args_to_check["rank"] == -1 else solver
+        sinkhorn_solver = solver.linear_solver if args_to_check["rank"] == -1 else solver
         lin_solver_args = gw_linear_solver_args if args_to_check["rank"] == -1 else gw_lr_linear_solver_args
         tmp_dict = args_to_check["linear_solver_kwargs"] if args_to_check["rank"] == -1 else args_to_check
         for arg, val in lin_solver_args.items():
@@ -342,7 +342,7 @@ class TestFGWProblem:
             },
         )
 
-        sinkhorn_solver = problem[("0", "1")].solver.solver.linear_ot_solver
+        sinkhorn_solver = problem[("0", "1")].solver.solver.linear_solver
 
         anderson = sinkhorn_solver.anderson
         assert isinstance(anderson, acceleration.AndersonAcceleration)
